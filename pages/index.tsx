0c5241b0bf30428b2dac32895e14e0b4c127eeab
OsoPooky <84725301+OsoPooky@users.noreply.github.com>
2023-02-26 19:42:07 -0300

    Update index.tsx
    en español para probar

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Face Photo Restorer</title>
+        <title>Para restaurar caras guacho</title>
       </Head>
 
       <Header />
@@ -22,21 +22,21 @@ const Home: NextPage = () => {
           rel="noreferrer"
           className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
         >
-          Used by over <span className="font-semibold">200,000</span> happy
-          users
+          Usado supuestamente por <span className="font-semibold">200,000</span> locos
+          felices
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
-          Restoring old photos{" "}
+          Para restaurar caras en fotos viejas{" "}
           <span className="relative whitespace-nowrap text-[#3290EE]">
             <SquigglyLines />
-            <span className="relative">using AI</span>
+            <span className="relative">usando IA</span>
           </span>{" "}
           for everyone.
         </h1>
 
         <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">
-          Have old and blurry face photos? Let our AI restore them so those
-          memories can live on. 100% free – restore your photos today.
+          Fotos antiguas y borrosas? Deja que la IA las restaure para que esas
+          memorias vivan algo más. Gratis por el momento.
         </p>
         <div className="flex justify-center space-x-4">
           <a
@@ -45,21 +45,21 @@ const Home: NextPage = () => {
             target="_blank"
             rel="noreferrer"
           >
-            Learn how it's built
+            Explicación de como lao armaron
           </a>
 
           <Link
             className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
             href="/restore"
           >
-            Restore your photos
+            Restaurar tus fotos
           </Link>
         </div>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-2 sm:flex-row flex-col">
               <div>
-                <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
+                <h2 className="mb-1 font-medium text-lg">Foto Original</h2>
                 <Image
                   alt="Original photo of my bro"
                   src="/michael.jpg"
@@ -69,7 +69,7 @@ const Home: NextPage = () => {
                 />
               </div>
               <div className="sm:mt-0 mt-8">
-                <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
+                <h2 className="mb-1 font-medium text-lg">Foto Restaurada</h2>
                 <Image
                   alt="Restored photo of my bro"
                   width={400}
